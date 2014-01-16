2ad8993f271198800ccee868480afb140f44dbbe
Christian Muehlhaeuser <muesli@gmail.com>
2014-01-16 02:35:50 +0100

    * Twitter-mod now retrieves all available tweets in the timeline. This has the downside that re-syncing / auto-updating will also try to do that. Needs a way to store the most recent retrieved ID and set it as 'since_id'. * A few style fixes.

--- mods/twitter.class.php
+++ mods/twitter.class.php
@@ -1,21 +1,21 @@
 <?php
 class twitter_reclaim_module extends reclaim_module {
     private static $shortname = 'twitter';
-    private static $apiurl = "http://api.twitter.com/1.1/statuses/user_timeline.json";
-    private static $count = 50;
-    private static $lang = 'de';
+    private static $apiurl = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+    private static $count = 200;
+    private static $lang = 'en';
     private static $post_format = 'status'; // or 'status', 'aside'
 
 //	const TWITTER_TWEET_TPL = '<blockquote class="twitter-tweet imported"><p>%s</p>%s&mdash; %s (<a href="https://twitter.com/%s/">@%s</a>) <a href="http://twitter.com/%s/status/%s">%s</a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
 
     public static function register_settings() {
         parent::register_settings(self::$shortname);
-        
+
         register_setting('reclaim-social-settings', 'twitter_username');
         register_setting('reclaim-social-settings', 'twitter_consumer_key');
         register_setting('reclaim-social-settings', 'twitter_consumer_secret');
         register_setting('reclaim-social-settings', 'twitter_user_token');
-        register_setting('reclaim-social-settings', 'twitter_user_secret');      
+        register_setting('reclaim-social-settings', 'twitter_user_secret');
     }
 
     public static function display_settings() {
@@ -23,7 +23,7 @@ class twitter_reclaim_module extends reclaim_module {
         <tr valign="top">
             <th colspan="2"><h3><?php _e('Twitter', 'reclaim'); ?></h3></th>
         </tr>
-<?php           
+<?php
         parent::display_settings(self::$shortname);
 ?>
         <tr valign="top">
@@ -45,7 +45,7 @@ class twitter_reclaim_module extends reclaim_module {
         <tr valign="top">
             <th scope="row"><?php _e('twitter user secret', 'reclaim'); ?></th>
             <td><input type="text" name="twitter_user_secret" value="<?php echo get_option('twitter_user_secret'); ?>" /></td>
-        </tr>              
+        </tr>
 <?php
     }
 
@@ -54,27 +54,42 @@ class twitter_reclaim_module extends reclaim_module {
         if (get_option('twitter_consumer_key') && get_option('twitter_consumer_secret') && get_option('twitter_user_token') && get_option('twitter_user_secret')) {
             parent::log(sprintf(__('BEGIN %s import', 'reclaim'), self::$shortname));
 
-            $tmhOAuth = new tmhOAuth(array(
-                'consumer_key' => get_option('twitter_consumer_key'),
-                'consumer_secret' => get_option('twitter_consumer_secret'),
-                'user_token' => get_option('twitter_user_token'),
-                'user_secret' => get_option('twitter_user_secret'),
-            ));
+            do {
+                $tmhOAuth = new tmhOAuth(array(
+                    'consumer_key' => get_option('twitter_consumer_key'),
+                    'consumer_secret' => get_option('twitter_consumer_secret'),
+                    'user_token' => get_option('twitter_user_token'),
+                    'user_secret' => get_option('twitter_user_secret'),
+                ));
 
-            $tmhOAuth->request('GET', self::$apiurl, array(
-                'lang' => self::$lang,
-                'count' => self::$count,
-                'screen_name' => get_option('twitter_username'),
-                'include_rts' => "false",
-                'exclude_replies' => "true",
-                'include_entities' => "true"
-            ), true);
+                $reqOptions = array(
+                    'lang' => self::$lang,
+                    'count' => self::$count,
+                    'screen_name' => get_option('twitter_username'),
+                    'include_rts' => "false",
+                    'exclude_replies' => "true",
+                    'include_entities' => "true"
+                );
+                if (isset($lastid)) {
+                    $reqOptions['max_id'] = $lastid;
+                }
+                $tmhOAuth->request('GET', self::$apiurl, $reqOptions, true);
 
-            if ($tmhOAuth->response['code'] == 200) {
-                $data = self::map_data(json_decode($tmhOAuth->response['response'], true));
-                parent::insert_posts($data);
-                update_option('reclaim_'.self::$shortname.'_last_update', current_time('timestamp'));
-            }
+                if ($tmhOAuth->response['code'] == 200) {
+                    parent::log(sprintf(__('Retrieved set of twitter messages: %s', 'reclaim'), $tmhOAuth->response['code']));
+
+                    $data = self::map_data(json_decode($tmhOAuth->response['response'], true));
+                    $reqOk = count($data) > 0;
+                    $lastid = $data[count($data)-1]["ext_guid"];
+
+                    parent::insert_posts($data);
+                    update_option('reclaim_'.self::$shortname.'_last_update', current_time('timestamp'));
+                }
+                else {
+                    $reqOk = false;
+                    parent::log(sprintf(__('GET failed with: %s', 'reclaim'), $tmhOAuth->response['code']));
+                }
+            } while ($reqOk);
             parent::log(sprintf(__('END %s import', 'reclaim'), self::$shortname));
         }
         else parent::log(sprintf(__('%s user data missing. No import was done', 'reclaim'), self::$shortname));
@@ -83,12 +98,11 @@ class twitter_reclaim_module extends reclaim_module {
     private static function map_data($rawData) {
         $data = array();
         $tags = array();
-        foreach($rawData as $entry){
-            
+        foreach($rawData as $entry) {
             $content = self::get_content($entry);
             $tags = self::get_hashtags($entry);
             // http://codex.wordpress.org/Function_Reference/wp_insert_post
-            $data[] = array(                
+            $data[] = array(
                 'post_author' => get_option(self::$shortname.'_author'),
                 'post_category' => array(get_option(self::$shortname.'_category')),
                 'post_date' => date('Y-m-d H:i:s', strtotime($entry["created_at"])),
@@ -104,13 +118,13 @@ class twitter_reclaim_module extends reclaim_module {
                 'tags_input' => $tags,
                 'ext_permalink' => 'http://twitter.com/'.get_option('twitter_username').'/status/'.$entry["id_str"],
                 'ext_image' => $content['image'],
-                'ext_guid' => $entry["id_str"]                
-            );            
-        }        
+                'ext_guid' => $entry["id_str"]
+            );
+        }
         return $data;
     }
 
-    private static function get_hashtags($entry){
+    private static function get_hashtags($entry) {
 		$tags = array();
         if (count($entry['entities']['hashtags'])) {
             foreach ($entry['entities']['hashtags'] as $hashtag) {
@@ -118,9 +132,9 @@ class twitter_reclaim_module extends reclaim_module {
             }
         }
         return $tags;
-	}    
+	}
 
-    private static function get_content($entry){
+    private static function get_content($entry) {
         $post_content = $entry['text'];
         $post_content = html_entity_decode($post_content); // ohne trim?
         //links einsetzen/auflösen
@@ -141,13 +155,13 @@ class twitter_reclaim_module extends reclaim_module {
             }
         }
         $post_content = preg_replace( "/\s((http|ftp)+(s)?:\/\/[^<>\s]+)/i", " <a href=\"\\0\" target=\"_blank\">\\0</a>",$post_content);
-        $post_content = preg_replace('/[@]+([A-Za-z0-9-_]+)/', '<a href="http://twitter.com/\\1" target="_blank">\\0</a>', $post_content );  
+        $post_content = preg_replace('/[@]+([A-Za-z0-9-_]+)/', '<a href="http://twitter.com/\\1" target="_blank">\\0</a>', $post_content );
 
 	        // Autolink hashtags (wordpress funktion)
         $post_content = preg_replace('/(^|[^0-9A-Z&\/]+)(#|\xef\xbc\x83)([0-9A-Z_]*[A-Z_]+[a-z0-9_\xc0-\xd6\xd8-\xf6\xf8\xff]*)/iu', '${1}<a href="http://twitter.com/search?q=%23${3}" title="#${3}">${2}${3}</a>', $post_content);
 
 	// original twitter embed code (more or less)
-        $embedcode = '<blockquote class="twitter-tweet imported"><p>'.$post_content.'</p>'.$image_html.'&mdash; '.$entry['user']['name'].' (<a href="https://twitter.com/'.$entry['user']['screen_name'].'/">@'.$entry['user']['screen_name'].'</a>) <a href="http://twitter.com/'.get_option('twitter_username').'/status/'.$entry["id_str"].'">'.date('d.m.Y H:i', strtotime($entry["created_at"])).'</a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';    
+        $embedcode = '<blockquote class="twitter-tweet imported"><p>'.$post_content.'</p>'.$image_html.'&mdash; '.$entry['user']['name'].' (<a href="https://twitter.com/'.$entry['user']['screen_name'].'/">@'.$entry['user']['screen_name'].'</a>) <a href="http://twitter.com/'.get_option('twitter_username').'/status/'.$entry["id_str"].'">'.date('d.m.Y H:i', strtotime($entry["created_at"])).'</a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
 	// if these are one's own tweets, there is no point to mark the username. also the date and time is supeficial.
 		$embedcode = '<blockquote class="twitter-tweet imported"><p>'.$post_content.'</p><div class="twimage">'.$image_html.'</div><span style="display: none;">&mdash; '.$entry['user']['name'].' (<a href="https://twitter.com/'.$entry['user']['screen_name'].'/">@'.$entry['user']['screen_name'].'</a>) <a href="http://twitter.com/'.get_option('twitter_username').'/status/'.$entry["id_str"].'">'.date('d.m.Y H:i', strtotime($entry["created_at"])).'</a></span><p class="twviewpost-twitter">(<a href="http://twitter.com/'.get_option('twitter_username').'/status/'.$entry["id_str"].'">'.__('View on Twitter', 'reclaim').'</a>)</p></blockquote>';
 
@@ -155,15 +169,15 @@ class twitter_reclaim_module extends reclaim_module {
 		setlocale (LC_ALL, get_bloginfo ( 'language' ) );
 		$embedcode = sprintf(
 				self::TWITTER_TWEET_TPL,
-				$post_content, 
-				$image_html, 
-				$entry['user']['name'], 
-				$entry['user']['screen_name'], 
-				$entry['user']['screen_name'], 
-				get_option('twitter_username'), 
-				$entry["id_str"], 
-				date('d.m.Y H:i', strtotime($entry["created_at"]))    
-//				date(get_option('date_format'), strtotime($entry["created_at"]))    
+				$post_content,
+				$image_html,
+				$entry['user']['name'],
+				$entry['user']['screen_name'],
+				$entry['user']['screen_name'],
+				get_option('twitter_username'),
+				$entry["id_str"],
+				date('d.m.Y H:i', strtotime($entry["created_at"]))
+//				date(get_option('date_format'), strtotime($entry["created_at"]))
 			);
 */
         $content = array(
@@ -171,7 +185,7 @@ class twitter_reclaim_module extends reclaim_module {
             'embedcode' => $embedcode,
             'image' => $image_url
         );
-        
-        return $content;        
+
+        return $content;
     }
 }
