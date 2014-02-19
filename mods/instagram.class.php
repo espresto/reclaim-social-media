42f4f27fff6618dac2298a5db7f21d2619de8ed0
Daniel nix <daniel.nix@gmail.com>
2014-02-19 22:27:03 +0100

    Update instagram.class.php

--- mods/instagram.class.php
+++ mods/instagram.class.php
@@ -104,7 +104,7 @@ class instagram_reclaim_module extends reclaim_module {
             <td><input type="text" type="password" name="instagram_client_secret" value="<?php echo get_option('instagram_client_secret'); ?>" />
             <input type="hidden" name="instagram_access_token" value="<?php echo get_option('instagram_access_token'); ?>" />
             <p class="description">
-             <p class="description">
+            
             <?php
             echo sprintf(__('Get your Instagram client and credentials <a href="%s">here</a>. ','reclaim'),'http://instagram.com/developer/');
             echo sprintf(__('Use <code>%s</code> as "Redirect URI"','reclaim'),plugins_url('reclaim/vendor/hybridauth/hybridauth/hybridauth/')); 
