dac8ec682faabc5335d1b89103735e9b25398798
Felix Schwenzel <dipl.ix@gmail.com>
2014-02-23 23:23:42 +0100

    * added ajax_resync_items() to youtube
    also
    * favs and activities count
    * seems to be a confusing setup (posts, favs, activity), but ajax sync
    can only loop through one API endpoint at a time. so we have 3 separate
    ajax calls, but still only one regular force resync for autoupdates
    * more generic type naming (posts instead of videos)

--- mods/youtube.class.php
+++ mods/youtube.class.php
@@ -18,7 +18,7 @@
 
 class youtube_reclaim_module extends reclaim_module {
     private static $timeout = 20;
-    private static $count = 50; // maximum: 50
+    private static $count = 10; // maximum: 50
     private static $apiurl     = "https://gdata.youtube.com/feeds/api/users/%s/uploads?alt=json&max-results=%s";
 	private static $fav_apiurl = "https://gdata.youtube.com/feeds/api/users/%s/favorites?alt=json&max-results=%s";
 	private static $activity_apiurl = "https://gdata.youtube.com/feeds/api/events?author=%s&v=2&key=%s&alt=json&max-results=%s";
@@ -27,7 +27,7 @@ class youtube_reclaim_module extends reclaim_module {
 
     public function __construct() {
         $this->shortname = 'youtube';
-        $this->has_ajaxsync = false;
+        $this->has_ajaxsync = true;
     }
 
     public function register_settings() {
@@ -36,6 +36,8 @@ class youtube_reclaim_module extends reclaim_module {
         register_setting('reclaim-social-settings', 'youtube_username');
         register_setting('reclaim-social-settings', 'youtube_favs_category');
         register_setting('reclaim-social-settings', 'youtube_import_favs');
+        register_setting('reclaim-social-settings', 'youtube_activity_category');
+        register_setting('reclaim-social-settings', 'youtube_import_activity');
     }
 
     public function display_settings() {
@@ -47,11 +49,24 @@ class youtube_reclaim_module extends reclaim_module {
         <tr valign="top">
             <th scope="row"><?php _e('Get Favs?', 'reclaim'); ?></th>
             <td><input type="checkbox" name="youtube_import_favs" value="1" <?php checked(get_option('youtube_import_favs')); ?> />
+            <?php if (get_option('youtube_import_favs')) { ?><input type="submit" class="button button-primary <?php echo $this->shortName(); ?>_resync_items" value="<?php _e('Resync favs with ajax', 'reclaim'); ?>" data-resync="{type:'favs'}" /><?php } ?>
+            <?php if (get_option('youtube_import_favs')) { ?><input type="submit" class="button button-secondary <?php echo $this->shortName(); ?>_count_all_items" value="<?php _e('Count with ajax', 'reclaim'); ?>" data-resync="{type:'favs'}" /><?php } ?>
             </td>
         </tr>
         <tr valign="top">
             <th scope="row"><?php _e('Category for Favs', 'reclaim'); ?></th>
             <td><?php wp_dropdown_categories(array('hierarchical' => 1, 'name' => 'youtube_favs_category', 'hide_empty' => 0, 'selected' => get_option('youtube_favs_category'))); ?></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row"><?php _e('Get activity? (i.e. videos you gave a thumbs up)', 'reclaim'); ?></th>
+            <td><input type="checkbox" name="youtube_import_activity" value="1" <?php checked(get_option('youtube_import_activity')); ?> />
+            <?php if (get_option('youtube_import_activity')) { ?><input type="submit" class="button button-primary <?php echo $this->shortName(); ?>_resync_items" value="<?php _e('Resync favs with ajax', 'reclaim'); ?>" data-resync="{type:'activity'}" /><?php } ?>
+            <?php if (get_option('youtube_import_activity')) { ?><input type="submit" class="button button-secondary <?php echo $this->shortName(); ?>_count_all_items" value="<?php _e('Count with ajax', 'reclaim'); ?>" data-resync="{type:'activity'}" /><?php } ?>
+            </td>
+        </tr>
+        <tr valign="top">
+            <th scope="row"><?php _e('Category for activity', 'reclaim'); ?></th>
+            <td><?php wp_dropdown_categories(array('hierarchical' => 1, 'name' => 'youtube_activity_category', 'hide_empty' => 0, 'selected' => get_option('youtube_activity_category'))); ?></td>
         </tr>
         <tr valign="top">
             <th scope="row"><?php _e('YouTube username', 'reclaim'); ?></th>
@@ -66,7 +81,7 @@ class youtube_reclaim_module extends reclaim_module {
             $rawData = json_decode($rawData, true);
 
             if (is_array($rawData)) {
-                $data = self::map_data($rawData, 'videos');
+                $data = self::map_data($rawData, 'posts');
                 parent::insert_posts($data);
                 update_option('reclaim_'.$this->shortname.'_videos_last_update', current_time('timestamp'));
             }
@@ -82,7 +97,7 @@ class youtube_reclaim_module extends reclaim_module {
                 }
                 else parent::log(sprintf(__('%s favs returned no data. No import was done', 'reclaim'), $this->shortname));
             }
-            if (get_option('youtube_import_favs') && get_option('google_api_key')) {
+            if (get_option('youtube_import_activity') && get_option('google_api_key')) {
                 $rawData = parent::import_via_curl(sprintf(self::$activity_apiurl, get_option('youtube_username'), get_option('google_api_key'), self::$count), self::$timeout);
                 $rawData = json_decode($rawData, true);
 
@@ -98,8 +113,97 @@ class youtube_reclaim_module extends reclaim_module {
         }
         else parent::log(sprintf(__('%s user data missing. No import was done', 'reclaim'), $this->shortname));
     }
+					
+    // called from ajax sync, calls import_tweet()
+    public function ajax_resync_items() {
+        $type = isset($_POST['type']) ? $_POST['type'] : 'posts';
+    	$offset = intval( $_POST['offset'] );
+    	$limit = intval( $_POST['limit'] );
+    	$count = intval( $_POST['count'] );
+    	$next_url = isset($_POST['next_url']) ? $_POST['next_url'] : null;
+    
+        self::log($this->shortName().' ' . $type . ' resync '.$offset.'-'.($offset + $limit).':'.$count);
+    	
+    	$return = array(
+    		'success' => false,
+    		'error' => '',
+			'result' => null
+    	);
+    	
+        if (get_option('youtube_username')) {
+            if ($next_url != '') {
+                $rawData = parent::import_via_curl($next_url, self::$timeout);
+            } else {
+                if ($type == 'posts') { $rawData = parent::import_via_curl(sprintf(self::$apiurl, get_option('youtube_username'), self::$count), self::$timeout); }
+                if ($type == 'favs') { $rawData = parent::import_via_curl(sprintf(self::$fav_apiurl, get_option('youtube_username'), self::$count), self::$timeout); }
+                if ($type == 'activity') { $rawData = parent::import_via_curl(sprintf(self::$activity_apiurl, get_option('youtube_username'), get_option('google_api_key'), self::$count), self::$timeout); }
+            }
 
-    private function map_data($rawData, $type = "videos") {
+            $rawData = json_decode($rawData, true);
+            if (is_array($rawData)) {
+                $data = self::map_data($rawData, $type);
+                parent::insert_posts($data);
+                update_option('reclaim_'.$this->shortname.'_'.$type.'_last_update', current_time('timestamp'));
+                
+                $new_next_url = null;
+                foreach($rawData['feed']['link'] as $link) {
+                   if ($link['rel'] == 'next') { $new_next_url = $link['href'];  }
+                }
+
+                
+                if (!isset($new_next_url)) { 
+                    //$return['error'] = sprintf(__('%s %s import done.', 'reclaim'), $type, $this->shortname); 
+                    $return['result'] = array(
+                        // when we're done, tell ajax script the number of imported items
+                        // and that we're done (offset == count)
+                        'offset' => $offset + sizeof($data),
+                        'count' => $offset + sizeof($data),
+                        'next_url' => null,
+                    );
+                } else {
+                    // youtube returns next_url without the api key - which we need to get 
+                    // activity. so lets add it, if we have it
+                    $google_api_key = get_option('google_api_key');
+                    $new_next_url .= isset($google_api_key) ? '&key='.$google_api_key : '';
+                    $return['result'] = array(
+                        'offset' => $offset + sizeof($data),
+                        // take the next pagination url instead of calculating
+                        // a self one
+                        'next_url' => $new_next_url,
+                    );
+                }
+                $return['success'] = true;
+            }
+            else $return['error'] = sprintf(__('%s returned no data. No import was done', 'reclaim'), $this->shortname);
+        }
+        else $return['error'] = sprintf(__('%s %s user data missing. No import was done', 'reclaim'), $this->shortname, $type);
+
+    	echo(json_encode($return));
+    	 
+    	die();
+    }
+
+    public function count_items( $type = 'posts' ) {
+        if (get_option('youtube_username')) {
+            // when it is called from a ajax-resync, post could be set...
+            // this name 'type' should be better choosen not to break other things
+            // in wordpress maybe mod_instagram_type
+            $type = (isset($_POST['type']) && $_POST['type'] != '') ? $_POST['type'] : $type;
+            if (!isset($type) || $type == 'posts') { $rawData = parent::import_via_curl(sprintf(self::$apiurl, get_option('youtube_username'), self::$count), self::$timeout); }
+            if ($type == 'favs') { $rawData = parent::import_via_curl(sprintf(self::$fav_apiurl, get_option('youtube_username'), self::$count), self::$timeout); }
+            if ($type == 'activity') { $rawData = parent::import_via_curl(sprintf(self::$activity_apiurl, get_option('youtube_username'), get_option('google_api_key'), 50).'&start-index=100', self::$timeout); } // max-results = 50 gets more accurate count, don't ask me why
+            $rawData = json_decode($rawData, true);
+            parent::log($type);
+            //parent::log(print_r($rawData, true));
+            return $rawData['feed']['openSearch$totalResults']['$t'];
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    private function map_data($rawData, $type = "posts") {
         $data = array();
         foreach($rawData['feed']['entry'] as $entry) {
             $content = self::get_content($entry, $type);
@@ -115,13 +219,14 @@ class youtube_reclaim_module extends reclaim_module {
             */
             $post_meta["_".$this->shortname."_link_id"] = $entry["id"];
             $post_meta["_post_generator"] = $this->shortname;
+            $post_meta["_reclaim_post_type"] = $type;
             // in case someone uses WordPress Post Formats Admin UI
             // http://alexking.org/blog/2011/10/25/wordpress-post-formats-admin-ui
             //$post_meta["_format_video_embed"]  = $entry["link"][0]['href'];
             /*
             *  set post meta galore end
             */
-            if ($type == "videos") {
+            if ($type == "posts") {
                 $title = $entry['title']['$t'];
                 $category = array(get_option($this->shortname.'_category'));
             } elseif ($type == "favs") {
@@ -131,7 +236,7 @@ class youtube_reclaim_module extends reclaim_module {
                 $video_id = $entry['yt$videoid']['$t'];
                 $id = 'http://gdata.youtube.com/feeds/api/videos/'.$video_id;
                 $title = sprintf(__('I rated a video on YouTube', 'reclaim'));
-                $category = array(get_option($this->shortname.'_favs_category'));
+                $category = array(get_option($this->shortname.'_activity_category'));
                 $content['embed_code'] = '<div class="ytembed yt"><iframe width="625" height="352" src="http://www.youtube.com/embed/'.$video_id.'" frameborder="0" allowfullscreen></iframe></div>';
                 $post_content = "[embed_code]";
                 $link = 'https://www.youtube.com/watch?v='.$video_id;
@@ -161,7 +266,7 @@ class youtube_reclaim_module extends reclaim_module {
         return $data;
     }
 
-    private function get_content($entry, $type="videos") {
+    private function get_content($entry, $type="posts") {
         $video_id = 0;
         $post_content = '';
         if (preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $entry["link"][0]['href'], $match)) {
